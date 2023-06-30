1dc197a0d45027ebc7b13ea37c508a2754e001f4
ricky <rpk2301@vt.edu>
2023-06-30 19:54:15 -0400

    I have added the first menu

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,48 @@
 package com.zipcodewilmington.scientificcalculator;
-
+import java.util.Scanner;
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
-    public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+    
+    
+    Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+     //   Console.println("Welcome to my calculator!");
+     //   String s = Console.getStringInput("Enter a string");
+     //   Integer i = Console.getIntegerInput("Enter an integer");
+     //   Double d = Console.getDoubleInput("Enter a double.");
+
+      //  Console.println("The user input %s as a string", s);
+      //  Console.println("The user input %s as a integer", i);
+      //  Console.println("The user input %s as a d", d);
+
+    DisplayMenu();
+
+
+
+
     }
+
+    public static void DisplayMenu()
+    {
+        StringBuilder welcome = new StringBuilder();
+        welcome.append("|----------------------------------------------------------------------------------------------------|" + '\n');
+        welcome.append("|                                   Welcome Menu                                  a) Addition        |" +  '\n');
+        welcome.append("|                                                                                 s) Subtraction     |" +  '\n');
+        welcome.append("|                                                                                 m) Multiplication  |" +  '\n');
+        welcome.append("|Please enter the character of the arithmetic function you would like to use:     d) Division        |" +  '\n');
+        welcome.append("|                                                                                 q) Square          |" +  '\n');
+        welcome.append("|                                                                                 r) Square Root     |" +  '\n');
+        welcome.append("|                                                                                 i) Inverse         |" +  '\n');
+        welcome.append("|----------------------------------------------------------------------------------------------------|"+    '\n');
+
+        String WelcomeMenu = welcome.toString();
+        System.out.println(WelcomeMenu);
+        scan.next()
+    }
+
+
 }
